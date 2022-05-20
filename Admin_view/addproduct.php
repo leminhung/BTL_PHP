7c447d29063f02adfadfefccc334c2db72d8fb90
unknown <leminhhungtabletennis@gmail.com>
2022-05-20 23:30:53 +0700

    Fix code of Nam

--- Admin_view/addproduct.php
+++ Admin_view/addproduct.php
@@ -26,26 +26,26 @@
     <link href="https://getbootstrap.com/docs/5.1/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
-    .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-    }
-
-    @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-            font-size: 3.5rem;
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
         }
-    }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
 
 
 
-    .col-md-4 {
+        .col-md-4 {
 
-        width: 93.333333% !important;
-    }
+            width: 93.333333% !important;
+        }
     </style>
 
 
@@ -54,17 +54,15 @@
 </head>
 
 <body>
-    <?php 
-        require_once "../database/config.php" ;
+    <?php
+    require_once "../database/config.php";
     ?>
 
-    
+
     <!-- HEADER -->
     <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
-        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
-            data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
-            aria-label="Toggle navigation">
+        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
@@ -159,8 +157,7 @@
                         </li>
                     </ul>
 
-                    <h6
-                        class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
                         <span>Saved reports</span>
                         <a class="link-secondary" href="#" aria-label="Add a new report">
                             <span data-feather="plus-circle"></span>
@@ -196,28 +193,26 @@
             </nav>
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
 
-            <?php
-            if (isset($_POST['add']))
-            {
-                $prd_categoryID = $_POST['prd_category'];
-                $prd_name = $_POST['prd_name'];
-                $prd_description = $_POST['prd_description'];
-                $prd_price = $_POST['prd_price'];
-                $prd_quantity = $_POST['prd_quantity'];
-                $prd_avatar = $_FILES['prd_avatar']['name'];
-                $prd_avatar_tmp = $_FILES['prd_avatar']['tmp_name'];
-                $path = './upload/';
-                $prd_sizeID = $_POST['prd_size'];
-                $sql_insert_product = mysqli_query($mysqli,"INSERT INTO products(category_id,product_name,product_description,product_price,product_quantity,size_id,product_image) VALUES ('$prd_categoryID','$prd_name','$prd_description','$prd_price','$prd_quantity','$prd_sizeID','$prd_avatar')");
-                move_uploaded_file($prd_avatar_tmp,$path.$prd_avatar);
-                header('location:./productList.php');
-                // echo '<script type="text/javascript">alert("Thêm sản phẩm thành công!!!");</script>';
-               
-                
-            }
-                
-            ?>
-            
+                <?php
+                if (isset($_POST['add'])) {
+                    $prd_categoryID = $_POST['prd_category'];
+                    $prd_name = $_POST['prd_name'];
+                    $prd_description = $_POST['prd_description'];
+                    $prd_price = $_POST['prd_price'];
+                    $prd_quantity = $_POST['prd_quantity'];
+                    $prd_avatar = $_FILES['prd_avatar']['name'];
+                    $prd_avatar_tmp = $_FILES['prd_avatar']['tmp_name'];
+                    $path = './upload/';
+                    $prd_sizeID = $_POST['prd_size'];
+                    $sql_insert_product = mysqli_query($mysqli, "INSERT INTO products(category_id,product_name,product_description,product_price,product_quantity,size_id,product_image) VALUES ('$prd_categoryID','$prd_name','$prd_description','$prd_price','$prd_quantity','$prd_sizeID','$prd_avatar')");
+                    move_uploaded_file($prd_avatar_tmp, $path . $prd_avatar);
+
+                    echo '<script type="text/javascript">alert("Thêm sản phẩm thành công!!!");</script>';
+                    // header('location: ./productList.php');
+                }
+
+                ?>
+
                 <form modelAttribute="products" method="post" action="" class="form-horizontal" enctype="multipart/form-data">
                     <fieldset>
                         <hidden path="id" />
@@ -226,18 +221,18 @@
                         <div style="width: 100%" class="form-group ">
                             <label class="col-md-4 control-label" for="category">CATEGORY (required)</label>
                             <?php
-                                $sql_danhmuc = mysqli_query($mysqli,"SELECT * FROM `categories`");
+                            $sql_danhmuc = mysqli_query($mysqli, "SELECT * FROM `categories`");
                             ?>
-                            
+
                             <div style="width: 93.333333% !important;" class="col-md-4">
                                 <select name="prd_category" class="form-control" id="category">
-                                <?php
-                                    while($row_danhmuc=mysqli_fetch_array($sql_danhmuc)){
+                                    <?php
+                                    while ($row_danhmuc = mysqli_fetch_array($sql_danhmuc)) {
                                     ?>
-                                    <option value="<?php echo $row_danhmuc['category_id'] ?>"><?php echo $row_danhmuc['category_name']?></option>
+                                        <option value="<?php echo $row_danhmuc['category_id'] ?>"><?php echo $row_danhmuc['category_name'] ?></option>
                                     <?php
                                     }
-                                ?>
+                                    ?>
                                 </select>
                             </div>
                         </div>
@@ -245,28 +240,25 @@
                         <div class="form-group">
                             <label class="col-md-4 control-label" for="title">PRODUCT NAME</label>
                             <div style="width: 93.333333% !important;" class="col-md-4">
-                                <input  id="title" name="prd_name" placeholder="Product name"
-                                    class="form-control input-md" type="text" />
+                                <input id="title" name="prd_name" placeholder="Product name" class="form-control input-md" type="text" />
                             </div>
                         </div>
                         <div class="form-group">
                             <label class="col-md-4 control-label" for="detailDescription">PRODUCT DESCRIPTION</label>
                             <div style="width: 93.333333% !important;" class="col-md-4 text-des">
-                                <textarea class="form-control" id="summernote"
-                                    name="prd_description"></textarea>
+                                <textarea class="form-control" id="summernote" name="prd_description"></textarea>
                             </div>
                         </div>
                         <script>
-                        $(document).ready(function() {
-                            $('#summernote').summernote();
-                        });
+                            $(document).ready(function() {
+                                $('#summernote').summernote();
+                            });
                         </script>
                         <!-- Text input-->
                         <div class="form-group">
                             <label class="col-md-4 control-label" for="Price">PRICE</label>
                             <div style="width: 93.333333% !important;" class="col-md-4">
-                                <input  id="Price" name="prd_price" placeholder="Price"
-                                    class="form-control input-md" type="text" />
+                                <input id="Price" name="prd_price" placeholder="Price" class="form-control input-md" type="text" />
                             </div>
                         </div>
                         <!-- Text input phan nay` chua bàn đến-->
@@ -282,8 +274,7 @@
                         <div class="form-group">
                             <label class="col-md-4 control-label" for="brand">QUANTITY</label>
                             <div style="width: 93.333333% !important;" class="col-md-4">
-                                <input name="prd_quantity" placeholder="Quantity"
-                                    class="form-control input-md" type="number" />
+                                <input name="prd_quantity" placeholder="Quantity" class="form-control input-md" type="number" />
                             </div>
                         </div>
                         <!-- phần thêm avatar   -->
@@ -298,18 +289,18 @@
                         <div style="width: 100%" class="form-group ">
                             <label class="col-md-4 control-label" for="category">Size</label>
                             <?php
-                                $sql_size = mysqli_query($mysqli,"SELECT * FROM `sizes`");
+                            $sql_size = mysqli_query($mysqli, "SELECT * FROM `sizes`");
                             ?>
-                            
+
                             <div style="width: 93.333333% !important;" class="col-md-4">
-                                <select name="prd_size" class="form-control" >
-                                <?php
-                                    while($row_size=mysqli_fetch_array($sql_size)){
+                                <select name="prd_size" class="form-control">
+                                    <?php
+                                    while ($row_size = mysqli_fetch_array($sql_size)) {
                                     ?>
-                                    <option value="<?php echo $row_size['size_id'] ?>"><?php echo $row_size['size_name']?></option>
+                                        <option value="<?php echo $row_size['size_id'] ?>"><?php echo $row_size['size_name'] ?></option>
                                     <?php
                                     }
-                                ?>
+                                    ?>
                                 </select>
                             </div>
                         </div>
@@ -331,11 +322,9 @@
 
     <script src="https://getbootstrap.com/docs/5.1/dist/js/bootstrap.bundle.min.js"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"
-        integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"
-        integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous">
     </script>
     <script src="dashboard.js"></script>
     <script src="dashboard.js"></script>
--- trangchu.php
+++ trangchu.php
@@ -9,13 +9,17 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    type="text/css" rel="stylesheet"
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="icon" href="./image/Free_Sample_By_Wix__1_-removebg-preview.png" type="image/icon type">
+    <title>Trang chủ</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
 <body>
     <div class="container-fluid">
         <?php
-            include './includes/navbar.php';
+        include './includes/navbar.php';
         ?>
         <!--slide-->
         <div style="margin-top: 154px;" class="row slide">
