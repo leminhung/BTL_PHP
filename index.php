4ac8e7684c83effe5b7e1fd420d35e6d23f6e252
unknown <leminhhungtabletennis@gmail.com>
2022-05-20 22:14:43 +0700

    Fix render images prod

--- index.php
+++ index.php
@@ -11,12 +11,10 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style.css">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <link rel="icon" href="./image/Free_Sample_By_Wix__1_-removebg-preview.png" type="image/icon type">
     <title>Phương Nam Sport</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
 <body>
@@ -29,53 +27,51 @@ session_start();
         <div class="row col-12" style="padding-left: 0px;">
             <div class="col-3 justify-content-start" style="padding-left: 0px;">
                 <div style="background-color: #333333 !important;" class="list-group list-group list-group-flush">
-                    <a style="background-color: #333333 !important; color: orange !important" href="#"
-                        class="list-group-item list-group-item-action list-category-item">Danh mục sản phẩm</a>
+                    <a style="background-color: #333333 !important; color: orange !important" href="#" class="list-group-item list-group-item-action list-category-item">Danh mục sản phẩm</a>
                     <?php
                     $sql = "SELECT * FROM `categories`";
                     $categories = $mysqli->query($sql);
                     ?>
                     <?php foreach ($categories as $category) { ?>
-                    <a href="?categoryId=<?php echo $category['category_id'] ?>"
-                        class="list-group-item list-group-item-action name-category-item"><?php echo $category['category_name'] ?></a>
+                        <a href="?categoryId=<?php echo $category['category_id'] ?>" class="list-group-item list-group-item-action name-category-item"><?php echo $category['category_name'] ?></a>
                     <?php } ?>
                 </div>
             </div>
             <?php
-                $trang =1;
-                if(isset($_GET['trang'])){
-                    $trang = $_GET['trang'];
-                }
+            $trang = 1;
+            if (isset($_GET['trang'])) {
+                $trang = $_GET['trang'];
+            }
 
-                $search ='';
-                if(isset($_GET['search'])){
-                    $search = strtoupper($_GET['search']);
-                }
-                //phan trang
-                $sql_so_san_pham ="SELECT COUNT(*) FROM products
+            $search = '';
+            if (isset($_GET['search'])) {
+                $search = strtoupper($_GET['search']);
+            }
+            //phan trang
+            $sql_so_san_pham = "SELECT COUNT(*) FROM products
                 WHERE UPPER(product_name) LIKE '%$search%'";
-                $mang_so_san_pham = $mysqli->query($sql_so_san_pham);
-                $kq_so_san_pham = mysqli_fetch_array($mang_so_san_pham);
-                $so_san_pham = $kq_so_san_pham['COUNT(*)'];
-                $so_san_pham_tren_1_trang = 9;
-                $so_trang = ceil($so_san_pham / $so_san_pham_tren_1_trang);
-                $boqua = $so_san_pham_tren_1_trang*($trang-1);
+            $mang_so_san_pham = $mysqli->query($sql_so_san_pham);
+            $kq_so_san_pham = mysqli_fetch_array($mang_so_san_pham);
+            $so_san_pham = $kq_so_san_pham['COUNT(*)'];
+            $so_san_pham_tren_1_trang = 9;
+            $so_trang = ceil($so_san_pham / $so_san_pham_tren_1_trang);
+            $boqua = $so_san_pham_tren_1_trang * ($trang - 1);
 
-                $sql= "";
-                if(isset($_GET['categoryId'])){
-                    $category_id = $_GET['categoryId'];
-                    $sql = "SELECT * FROM products
+            $sql = "";
+            if (isset($_GET['categoryId'])) {
+                $category_id = $_GET['categoryId'];
+                $sql = "SELECT * FROM products
                     WHERE
                     (category_id = $category_id)
                     LIMIT $so_san_pham_tren_1_trang offset $boqua";
-                }else {
-                    $sql = "SELECT * FROM products
+            } else {
+                $sql = "SELECT * FROM products
                     WHERE
                     UPPER(product_name) REGEXP BINARY '^$search'
                     LIMIT $so_san_pham_tren_1_trang offset $boqua";
-                }
-                $products = $mysqli->query($sql);
-                //UPPER(product_name) LIKE '%$search%'
+            }
+            $products = $mysqli->query($sql);
+            //UPPER(product_name) LIKE '%$search%'
             ?>
             <div class="col-9 content">
                 <div class="row">
@@ -100,46 +96,43 @@ session_start();
                 <!-- danh sach sp -->
                 <div class="row list-product">
                     <?php
-                    if (is_array($products) || is_object($products)){
-                        foreach($products as $product) {
+                    if (is_array($products) || is_object($products)) {
+                        foreach ($products as $product) {
                     ?>
-                    <div class="col-lg-3 col-md-4 col-sm-6  ">
-                        <div style="margin-bottom: 10px !important;" class="card card-custom">
-                            <span class="ico-sale">-<?php echo $product['product_sale'] ?>%</span>
-                            <img class="card-img-top link" src="<?php echo $product['product_image']?>"
-                                alt="Card image cap">
-                            <div style="padding: 0 1px !important;" class="card-body">
-                                <a class="card-title product-title"
-                                    href="./chitiet.php"><?php echo $product['product_name']?></a>
-                                <br>
-                                <div class="price">
-                                    <span class="card-text old-price"><?php echo $product['product_price']?>đ</span>
-                                    <span class="card-text new-price">
-                                        <?php
-                                        $newprice = $product['product_price'];
-                                        if($product['product_sale'] >0){
-                                            $newprice = $product['product_price'] - $product['product_price']*$product['product_sale']/100;
-                                            echo ($newprice);
-                                        }
-                                        else {
-                                             echo $newprice;
-                                        }
-                                        ?>đ
-                                    </span>
+                            <div class="col-lg-3 col-md-4 col-sm-6  ">
+                                <div style="margin-bottom: 10px !important;" class="card card-custom">
+                                    <span class="ico-sale">-<?php echo $product['product_sale'] ?>%</span>
+                                    <img class="card-img-top link" src="./Admin_view/upload/<?php echo $product['product_image'] ?>" alt="Card image cap">
+                                    <div style="padding: 0 1px !important;" class="card-body">
+                                        <a class="card-title product-title" href="./chitiet.php"><?php echo $product['product_name'] ?></a>
+                                        <br>
+                                        <div class="price">
+                                            <span class="card-text old-price"><?php echo $product['product_price'] ?>đ</span>
+                                            <span class="card-text new-price">
+                                                <?php
+                                                $newprice = $product['product_price'];
+                                                if ($product['product_sale'] > 0) {
+                                                    $newprice = $product['product_price'] - $product['product_price'] * $product['product_sale'] / 100;
+                                                    echo ($newprice);
+                                                } else {
+                                                    echo $newprice;
+                                                }
+                                                ?>đ
+                                            </span>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <?php }} ?>
+                    <?php }
+                    } ?>
                 </div>
                 <hr>
                 <nav style="margin: auto; color: orange !important;" class="col-4" aria-label="Page navigation example">
                     <ul class="pagination">
-                        <?php for($i=1; $i<=$so_trang; $i++){ ?>
-                        <li class="page-item"><a style="color: orange !important;" class="page-link"
-                                href="?trang=<?php echo $i ?>&timkiem=<?php echo $search ?>"><?php echo $i?></a>
-                        </li>
-                        <?php }?>
+                        <?php for ($i = 1; $i <= $so_trang; $i++) { ?>
+                            <li class="page-item"><a style="color: orange !important;" class="page-link" href="?trang=<?php echo $i ?>&timkiem=<?php echo $search ?>"><?php echo $i ?></a>
+                            </li>
+                        <?php } ?>
                     </ul>
                 </nav>
             </div>
@@ -259,14 +252,11 @@ session_start();
     </footer>
     <!-- Footer -->
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
     </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
     </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
     <script src="./script.js"></script>
     <script src="./OwlCarousel2-2.3.4/src/js/owl.carousel.js"></script>
